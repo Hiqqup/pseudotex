9ce5dde5e9220e3d48e9010477484158ed96e968
hiqqup <jubu6000@gmail.com>
2023-11-08 23:35:23 +0100

    implement current path and append filename

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include "uthash.h"
+#include <limits.h>
 #include <stdio.h> /* printf */
 #include <stdlib.h> /* malloc */
 #include <string.h> /* strcpy */
 #include <unistd.h>
+
 struct keyword {
     const char* key; /* key */
     char* color;
@@ -28,13 +30,29 @@ struct keyword* setDefaultKeywords()
     keys = addKeyword("return", "magenta", keys);
     return keys;
 }
+char* fullInputDir(char* filename)
+{
+    char currentDir[PATH_MAX];
+    if (getcwd(currentDir, sizeof(currentDir)) == NULL) {
+        perror("getcwd() error");
+        exit(1);
+    }
+    char* dirPtr = malloc((strlen(currentDir) + strlen(filename) + 1) * sizeof(char));
+    for (int i = 0; i < strlen(currentDir); i++) {
+        dirPtr[i] = currentDir[i];
+    }
+    dirPtr[strlen(currentDir)] = '/';
+    for (int i = 0; i <= strlen(filename); i++) {
+        dirPtr[strlen(currentDir) + i + 1] = filename[i];
+    }
+    return dirPtr;
+}
 int main(int argc, char* argv[])
 {
     struct keyword* tmp;
     struct keyword* keys = setDefaultKeywords();
 
-    char* getcwd(char* buf, size_t size);
-
+    printf("cwd: %s\n", fullInputDir("input"));
     /* from the example */
     HASH_FIND_STR(keys, "for", tmp);
     if (tmp)
